57518ab184111ddccdd202d751c0eef8deaaa06c
rikuriku1248 <nishino2793@ezweb.ne.jp>
2018-01-28 00:10:31 +0900

    パターンムーブメント

---
+++ PatternMovement/PatternMovement1.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatternMovement1 : MonoBehaviour {
+
+	private float[] rootX = new float[300];//経路のx成分を格納する配列
+	private float[] rootZ = new float[300];//経路のz成分を格納する配列
+	private int maxPathLength = 300;
+	int CurrentIndex = 0;
+	int pathSize = 0;
+
+	void Start () {
+		InitPathArray ();
+		//BuildPath (0, 0, 5, 4);
+		//BuildPath (5, 4, 0, 0);
+		BuildPath (10, 3, 18, 3);
+		BuildPath (18, 3, 18, 12);
+		BuildPath (18, 12, 10, 12);
+		BuildPath (10, 12, 10, 3);
+		Normalizepattern ();
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if(CurrentIndex == pathSize){
+				//インデックスの初期化
+				CurrentIndex = 0;
+			}
+
+			Debug.Log ("[" + rootX[CurrentIndex] + "]" + ", " + "[" + rootZ[CurrentIndex] + "]" + ", pathSize=" + pathSize);
+			//this.transform.Rotate (new Vector3 (0, Pattern [CurrentIndex].turnRight, 0));
+			//this.transform.Rotate (new Vector3 (0, Pattern [CurrentIndex].turnLeft, 0));
+			//this.transform.position (rootX [CurrentIndex], 0, rootZ [CurrentIndex]);
+			this.transform.position = new Vector3(rootX [CurrentIndex], 0, rootZ [CurrentIndex]);
+
+			//インデックスのインクリメント
+			CurrentIndex++;
+		}
+	}
+
+	//経路配列の初期化
+	private void InitPathArray(){
+		for(int i = 0; i < maxPathLength; i++){
+			rootX [i] = -1;
+			rootZ [i] = -1;
+		}
+	}
+
+	private void BuildPath(float startX, float startZ, float endX, float endZ){
+		float nextX = startX;
+		float nextZ = startZ;
+		float deltaX = endX - startX;
+		float deltaZ = endZ - startZ;
+		float stepX, stepZ;
+		float fraction;
+		int currentStep = 0;
+
+		//経路の初期化
+		for(int i = 0; i < maxPathLength; i++){
+			if(rootX [i] == -1 && rootZ [i] == -1){
+				currentStep = i;
+				break;
+			}
+		}
+
+		//currentStep = 0;
+
+		//各成分の差から移動方向を決定
+		if(deltaX < 0){
+			stepX = -1;
+		}else{
+			stepX = 1;
+		}
+		if(deltaZ < 0){
+			stepZ = -1;
+		}else{
+			stepZ = 1;
+		}
+
+		deltaX = Mathf.Abs (deltaX * 2);
+		deltaZ = Mathf.Abs (deltaZ * 2);
+
+
+		if(deltaX > deltaZ){//x軸の方が長い時
+			fraction = deltaZ - deltaX / 2;
+			while(nextX != endX){
+				if(fraction >= 0){
+					nextZ += stepZ;
+					fraction -= deltaX;
+				}
+				nextX += stepX;
+				fraction += deltaZ;
+				rootX [currentStep] = nextX;
+				rootZ [currentStep] = nextZ;
+				currentStep++;
+			}
+		}else{//z軸の方が長い時
+			fraction = deltaX - deltaZ / 2;
+			while(nextZ != endZ){
+				if(fraction >= 0){
+					nextX += stepX;
+					fraction -= deltaZ;
+				}
+				nextZ += stepZ;
+				fraction += deltaX;
+
+				rootX [currentStep] = nextX;
+				rootZ [currentStep] = nextZ;
+				currentStep++;
+			}
+		}
+	}
+
+	private void Normalizepattern(){
+		int i;
+
+		float originX = rootX [0];
+		float originZ = rootZ [0];
+		//float originX = transform.position.x;
+		//float originZ = transform.position.z;
+
+		for(i = 0; i < maxPathLength; i++){
+			if(rootX [i] == -1 && rootZ [i] == -1){
+				pathSize = i;
+				break;
+			}
+		}
+
+		for(i = 0; i < pathSize; i++){
+			rootX [i] = rootX [i] - originX;
+			rootZ [i] = rootZ [i] - originZ;
+		}
+	}
+}
---
+++ PatternMovement/PatternMovement_normal.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatternMovement_normal : MonoBehaviour {
+
+	ControllData[] Pattern;
+
+	int CurrentIndex = 0;
+
+	//制御命令構造体
+	struct ControllData{
+		public float turnRight;
+		public float turnLeft;
+		public float stepForward;
+		public float stepBackward;
+	}
+
+	void Start () {
+		Pattern = new ControllData[4];
+		for(int i = 0; i < Pattern.Length; i++){
+			Pattern[0] = new ControllData();
+		}
+		InitPattern ();
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if(CurrentIndex == 4){
+				//インデックスの初期化
+				CurrentIndex = 0;
+			}
+
+			this.transform.Rotate (new Vector3 (0, Pattern [CurrentIndex].turnRight, 0));
+			this.transform.Rotate (new Vector3 (0, Pattern [CurrentIndex].turnLeft, 0));
+			this.transform.Translate (0, 0, Pattern [CurrentIndex].stepForward);
+			this.transform.Translate (0, 0, Pattern [CurrentIndex].stepBackward);
+
+			//インデックスのインクリメント
+			CurrentIndex++;
+		}
+	}
+		
+
+	//パターンの初期化
+	public void InitPattern(){
+		Pattern [0].turnRight = 0;
+		Pattern [0].turnLeft = 0;
+		Pattern [0].stepForward = 1;
+		Pattern [0].stepBackward = 0;
+
+		Pattern [1].turnRight = 90;
+		Pattern [1].turnLeft = 0;
+		Pattern [1].stepForward = 0;
+		Pattern [1].stepBackward = 0;
+
+		Pattern [2].turnRight = 0;
+		Pattern [2].turnLeft = 0;
+		Pattern [2].stepForward = 1;
+		Pattern [2].stepBackward = 0;
+
+		Pattern [3].turnRight = 90;
+		Pattern [3].turnLeft = 0;
+		Pattern [3].stepForward = 0;
+		Pattern [3].stepBackward = 0;
+
+	}
+}
